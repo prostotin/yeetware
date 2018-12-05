9c85a96f0bd3230123acd580eb910dbd70f4212e
Oleksandr Prostotin <37674127+olexprost@users.noreply.github.com>
2018-12-04 20:41:17 -0500

    Formatting

--- trigger.cpp
+++ trigger.cpp
@@ -5,15 +5,7 @@ public:
 			if (crosshairID > 0) {			
 				int shoot = 6; //what we will write to dwForceAttack
 				WriteProcessMemory(processHandle, (LPVOID)(clientDLL + hazedumper::signatures::dwForceAttack), &shoot, sizeof(shoot), NULL); // write the shooting action, we write 6 to both +attack and -attack
-				Sleep(10); //sleep to save cpu
-			
+				Sleep(10); //sleep to save cpu	
 			}
-		
 	}
-
-
-		
-	
-
-
 };
