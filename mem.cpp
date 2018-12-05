b55b49c5820009b9bc161fae4f103ec06da70102
Oleksandr Prostotin <37674127+olexprost@users.noreply.github.com>
2018-12-04 20:38:46 -0500

    Formatting

--- mem.cpp
+++ mem.cpp
@@ -5,87 +5,77 @@
 using namespace std;
 
 class MemoryManagement {
-	DWORD clientDLL;
-	HANDLE processHandle;
-public:
-	MemoryManagement(DWORD clientDLL)  {
-		this->clientDLL = clientDLL;
-		
-	}
+   DWORD clientDLL;
+   HANDLE processHandle;
+   public:
+      MemoryManagement(DWORD clientDLL) {
+         this - > clientDLL = clientDLL;
 
+      }
 
-	DWORD getLocalPlayer(DWORD clientDLL, HANDLE processHandle) {
-		DWORD localPlayer;
-		DWORD cliDLL = clientDLL;
-		
-		
-		
-		ReadProcessMemory(processHandle, (LPCVOID)(cliDLL + hazedumper::signatures::dwLocalPlayer), &localPlayer, sizeof(localPlayer), NULL);
-		if (localPlayer == NULL) {
-			return NULL;
-		}
-		
-		return localPlayer;
-	}
-	DWORD getCrosshairID(DWORD localPlayer, HANDLE processHandle) {
-		int crosshairID = 0;
-		ReadProcessMemory(processHandle, (LPCVOID)(localPlayer + hazedumper::netvars::m_iCrosshairId), &crosshairID, sizeof(crosshairID), NULL);
-		return crosshairID;
-	}
+   DWORD getLocalPlayer(DWORD clientDLL, HANDLE processHandle) {
+      DWORD localPlayer;
+      DWORD cliDLL = clientDLL;
 
-	int getFlags(DWORD localPlayer, HANDLE processHandle) {
-		int onGround ;
-		ReadProcessMemory(processHandle, (LPCVOID)(localPlayer + hazedumper::netvars::m_fFlags), &onGround, sizeof(onGround), NULL);
-		if (onGround == NULL) {
-		
-		}
-		
-		return  onGround;
-	}
+      ReadProcessMemory(processHandle, (LPCVOID)(cliDLL + hazedumper::signatures::dwLocalPlayer), & localPlayer, sizeof(localPlayer), NULL);
+      if (localPlayer == NULL) {
+         return NULL;
+      }
 
-	short int getCurrentWeapon(DWORD localPlayer, DWORD clientDLL, HANDLE processHandle) {
-		DWORD activeWeapon;
-		ReadProcessMemory(processHandle, (LPCVOID)(localPlayer + hazedumper::netvars::m_hActiveWeapon), &activeWeapon, sizeof(activeWeapon), NULL);
+      return localPlayer;
+   }
+   DWORD getCrosshairID(DWORD localPlayer, HANDLE processHandle) {
+      int crosshairID = 0;
+      ReadProcessMemory(processHandle, (LPCVOID)(localPlayer + hazedumper::netvars::m_iCrosshairId), & crosshairID, sizeof(crosshairID), NULL);
+      return crosshairID;
+   }
 
-		DWORD weaponEntity = activeWeapon & 0xFFF;
+   int getFlags(DWORD localPlayer, HANDLE processHandle) {
+      int onGround;
+      ReadProcessMemory(processHandle, (LPCVOID)(localPlayer + hazedumper::netvars::m_fFlags), & onGround, sizeof(onGround), NULL);
+      if (onGround == NULL) {
 
-		DWORD currentWeapon;
-		ReadProcessMemory(processHandle, (LPCVOID)(clientDLL + hazedumper::signatures::dwEntityList + (weaponEntity - 1) * 0x10), &currentWeapon, sizeof(currentWeapon), NULL);
+      }
 
-		short int weaponID;
-		ReadProcessMemory(processHandle, (LPCVOID)(currentWeapon + hazedumper::netvars::m_iItemDefinitionIndex), &weaponID, sizeof(weaponID), NULL);
-		return weaponID;
+      return onGround;
+   }
 
-	}
+   short int getCurrentWeapon(DWORD localPlayer, DWORD clientDLL, HANDLE processHandle) {
+      DWORD activeWeapon;
+      ReadProcessMemory(processHandle, (LPCVOID)(localPlayer + hazedumper::netvars::m_hActiveWeapon), & activeWeapon, sizeof(activeWeapon), NULL);
 
-	int getMyTeam(HANDLE processHandle, DWORD localPlayer) {
-		int myTeam;
-		ReadProcessMemory(processHandle, (LPCVOID)(localPlayer + 0xF0), &myTeam, sizeof(myTeam), NULL);
+      DWORD weaponEntity = activeWeapon & 0xFFF;
 
-		return myTeam;
-	}
+      DWORD currentWeapon;
+      ReadProcessMemory(processHandle, (LPCVOID)(clientDLL + hazedumper::signatures::dwEntityList + (weaponEntity - 1) * 0x10), & currentWeapon, sizeof(currentWeapon), NULL);
 
-		int getEnemyTeam(HANDLE processHandle, DWORD entity) {
-			int enemyTeam;
-		ReadProcessMemory(processHandle, (LPCVOID)(entity + 0xF0), &enemyTeam, sizeof(enemyTeam), NULL);
+      short int weaponID;
+      ReadProcessMemory(processHandle, (LPCVOID)(currentWeapon + hazedumper::netvars::m_iItemDefinitionIndex), & weaponID, sizeof(weaponID), NULL);
+      return weaponID;
 
-		return enemyTeam;
-	}
-	
-	 
-	void main()
+   }
 
-	 {
-		DWORD localPlayer = getLocalPlayer(clientDLL, processHandle);
-		//cout << localPlayer;
-	}
+   int getMyTeam(HANDLE processHandle, DWORD localPlayer) {
+      int myTeam;
+      ReadProcessMemory(processHandle, (LPCVOID)(localPlayer + 0xF0), & myTeam, sizeof(myTeam), NULL);
 
+      return myTeam;
+   }
 
+   int getEnemyTeam(HANDLE processHandle, DWORD entity) {
+      int enemyTeam;
+      ReadProcessMemory(processHandle, (LPCVOID)(entity + 0xF0), & enemyTeam, sizeof(enemyTeam), NULL);
 
+      return enemyTeam;
+   }
 
+   void main()
 
+   {
+      DWORD localPlayer = getLocalPlayer(clientDLL, processHandle);
+      //cout << localPlayer;
+   }
 
 };
-
 
 
